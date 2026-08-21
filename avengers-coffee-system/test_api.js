dae8ea27cf37adaec30367543b91aa321e914334
your name <ankudo1234@gmail.com>
2026-08-21 09:41:10 +0700

    feat (file): clean cac file k can thiet

--- avengers-coffee-system/test_api.js
+++
@@ -1,4 +0,0 @@
-const axios = require('axios');
-axios.get('http://localhost:3000/menu/san-pham')
-  .then(res => console.log("DATA:", JSON.stringify(res.data).substring(0, 500)))
-  .catch(err => console.error("ERROR:", err.message));
--- get_logs.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-import os
-
-with open('kafka_logs.txt', 'w') as f:
-    subprocess.run(['docker', 'logs', 'avengers_kafka'], stdout=f, stderr=subprocess.STDOUT)
